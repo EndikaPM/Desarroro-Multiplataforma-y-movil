4fe060ec86c96635e4d08e92e143ebc6d426c61c
EndikaPM <endika.perez.mas@gmail.com>
2025-09-28 17:25:51 +0200

    arreglo toast enviar y recibir

--- app/src/main/java/com/example/enviarrecivir/MainActivity.java
+++ app/src/main/java/com/example/enviarrecivir/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.enviarrecivir;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -10,15 +16,34 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int REQUEST_CODE = 10;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button button = findViewById(R.id.button_send);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent envio = new Intent(MainActivity.this, MainActivity2.class);
+                envio.putExtra("Nombre","Endika");
+                startActivityForResult(envio,REQUEST_CODE);
+            }
         });
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK && requestCode == REQUEST_CODE){
+            if(data.hasExtra("Retorno")) {
+                String mensaje = data.getStringExtra("Retorno");
+                Toast.makeText(this, mensaje, Toast.LENGTH_SHORT).show();
+            }
+        }
     }
 }
